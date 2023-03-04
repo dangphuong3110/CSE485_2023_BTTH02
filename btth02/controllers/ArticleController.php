61ef9b7947ba8ee6273569c5c02041018764fe22
dangphuong3110 <nguyendangphuong31102002@gmail.com>
2023-03-04 09:42:46 +0700

    cấu trúc lại homepage và article_detail theo mô hình MVC

--- btth02/controllers/ArticleController.php
+++ btth02/controllers/ArticleController.php
@@ -1,4 +1,5 @@
 <?php
+include('services/ArticleService.php');
 class ArticleController{
     // Hàm xử lý hành động index
     public function index(){
@@ -20,5 +21,21 @@ class ArticleController{
         // echo "Tương tác với Services/Models from Article";
         // Nhiệm vụ 2: Tương tác với View
         include("views/article/list_article.php");
+    }
+
+    public function detail(){
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+        if(!$id){
+            die('<h1>Không có bài viết này!</h1>');
+        }
+
+        $articleService = new ArticleService();
+        $articleDetail = $articleService->getArticleDetail($id);
+        if(!$articleDetail){
+            die('<h1>Không có bài viết này!</h1>');
+        }
+
+        $title = $articleDetail->getTen_bhat(). ' - ' . $articleDetail->getTen_tgia();
+        include('views/article/article_detail.php');
     }
 }
