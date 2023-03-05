6c9449465d216cb3b281c40fde5da34beb93b29f
NguyenPhuongTrinh <2051063817.e.tlu.edu.vn>
2023-03-05 16:05:13 +0700

    sua mat khau taikhoan trong db va sua lai ten db

--- btth02/configs/DBConnection.php
+++ btth02/configs/DBConnection.php
@@ -4,7 +4,7 @@ class DBConnection{
 
     public function __construct(){
         try {
-            $this->conn = new PDO('mysql:host=localhost;dbname=trinh;port=3306', 'root','');
+            $this->conn = new PDO('mysql:host=localhost;dbname=btth02_cse485;port=3306', 'root','');
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
