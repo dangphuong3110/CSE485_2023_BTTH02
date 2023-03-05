d972ca3c90ef2d8deca6e3ca0b25bd262b6ffc85
VanHieu3102 <nguyenvanhieu301102@gmail.com>
2023-03-05 09:48:36 +0700

    sua controller

--- btth02/controllers/CategoryController.php
+++ btth02/controllers/CategoryController.php
@@ -1,20 +1,46 @@
 <?php
-    include('/services/CategoryServices.php');
-    class CategoryServices {
+    include('services/CategoryServices.php');
+    class CategoryController {
         public function index()
         {
             //Lấy dữ liệu
-            $categoryServices = new categoryServices();
-            $category = $categoryServices->getAllCategory();
+            $categoryService = new categoryServices();
+            $category = $categoryService->getAllCategory();
             //Truyền tới view
-            include('views/category/add_category.php');
-            include('views/category/delete_category.php');
-            include('views/category/edit_category.php');
+            include('views/category/category.php');
         }
 
-        public function add()
+        public function edit($id)
         {
-            
+            $categoryService = new CategoryService();
+            $category = $categoryService->editCategory($id);
+            include("view/category/edit_category.php");
+        }
+
+        public function update()
+        {
+            $categoryService = new CategoryService();
+            $categories = $categoryService->updateCategory($_POST['txtCatId'],$POST['txtCatName']);
+            include("view/category/category.php");
+        }
+
+        public function create()
+        {
+            include("view/category/add_category.php");
+        }
+
+        public function store()
+        {
+            $categoryService = new CategoryService();
+            $categories = $categoryService->storeCategory($_POST['txtCatName']);
+            include("view/category/category.php");
+        }
+
+        public function delete()
+        {
+            $categoryService = new CategoryService();
+            $categories = $categoryService->deleteCategory($_GET['id']);
+            include("view/category/categpry.php");
         }
     }
 ?>
--- btth02/models/Category.php
+++ btth02/models/Category.php
@@ -1,91 +1,40 @@
 <?php
-class CategoryServices{
+class Category{
     //Thuộc tính 
-
     private $ma_tloai;
     private $ten_tloai;
     private $so_luong;
 
-    public function __construct($ma_tloai, $ten_tloai, $so_luong)
-    {
+    public function __construct($ma_tloai, $ten_tloai, $so_luong){
         $this->ma_tloai = $ma_tloai;
         $this->ten_loai = $ten_tloai;
         $this->so_luong = $so_luong;
     }
 
-    /**
-     * Get the value of ma_tloai
-     */ 
-    public function getMa_tloai()
-    {
+    public function getMa_tloai() {
         return $this->ma_tloai;
     }
-
-    /**
-     * Set the value of ma_tloai
-     *
-     * @return  self
-     */ 
-    public function setMa_tloai($ma_tloai)
-    {
+ 
+    public function setMa_tloai($ma_tloai) {
         $this->ma_tloai = $ma_tloai;
-
         return $this;
     }
-
-    /**
-     * Get the value of ten_tloai
-     */ 
-    public function getTen_tloai()
-    {
+ 
+    public function getTen_tloai() {
         return $this->ten_tloai;
     }
-
-    /**
-     * Set the value of ten_tloai
-     *
-     * @return  self
-     */ 
-    public function setTen_tloai($ten_tloai)
-    {
+ 
+    public function setTen_tloai($ten_tloai) {
         $this->ten_tloai = $ten_tloai;
-
         return $this;
     }
 
-    /**
-     * Get the value of so_luong
-     */ 
-    public function getSo_luong()
-    {
+    public function getSo_luong() {
         return $this->so_luong;
     }
 
-    /**
-     * Set the value of so_luong
-     *
-     * @return  self
-     */ 
-    public function setSo_luong($so_luong)
-    {
+    public function setSo_luong($so_luong) {
         $this->so_luong = $so_luong;
-
         return $this;
     }
-
-    // public function getCategories() {
-    //     // trả về danh sách danh mục sản phẩm
-    //   }
-    
-    //   public function addCategory($categoryName) {
-    //     // thêm một danh mục sản phẩm mới
-    //   }
-    
-    //   public function updateCategory($categoryId, $newCategoryName) {
-    //     // sửa đổi tên của một danh mục sản phẩm
-    //   }
-    
-    //   public function deleteCategory($categoryId) {
-    //     // xoá một danh mục sản phẩm
-    //   }
 }
