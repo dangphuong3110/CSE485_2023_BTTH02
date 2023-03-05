6fd125e869d49d0cc78d09ef2463010a0674609c
VanHieu3102 <nguyenvanhieu301102@gmail.com>
2023-03-05 10:37:25 +0700

    services category

--- btth02/models/Category.php
+++ btth02/models/Category.php
@@ -3,38 +3,43 @@ class Category{
     //Thuộc tính 
     private $ma_tloai;
     private $ten_tloai;
-    private $so_luong;
+    private $SLBaiViet;
 
-    public function __construct($ma_tloai, $ten_tloai, $so_luong){
+    public function __construct($ma_tloai, $ten_tloai, $SLBaiViet){
         $this->ma_tloai = $ma_tloai;
         $this->ten_loai = $ten_tloai;
-        $this->so_luong = $so_luong;
+        $this->SLBaiViet= $SLBaiViet;
     }
 
     public function getMa_tloai() {
         return $this->ma_tloai;
     }
- 
-    public function setMa_tloai($ma_tloai) {
+
+    public function setMa_tloai($ma_tloai)
+    {
         $this->ma_tloai = $ma_tloai;
         return $this;
     }
  
-    public function getTen_tloai() {
+    public function getTen_tloai()
+    {
         return $this->ten_tloai;
     }
  
-    public function setTen_tloai($ten_tloai) {
+    public function setTen_tloai($ten_tloai)
+    {
         $this->ten_tloai = $ten_tloai;
         return $this;
     }
 
-    public function getSo_luong() {
-        return $this->so_luong;
+    public function getSLBaiViet()
+    {
+        return $this->SLBaiViet;
     }
 
-    public function setSo_luong($so_luong) {
-        $this->so_luong = $so_luong;
+    public function setSLBaiViet($SLBaiViet)
+    {
+        $this->SLBaiViet = $SLBaiViet;
         return $this;
     }
 }
